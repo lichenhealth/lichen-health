85267964621e6e0fb5073a56a92f53a9708e5a2c
lichenhealth <connect@lichen.health>
2026-05-31 07:31:11 -0600

    Faithful redesign matching WordPress site

---
+++ src/data/images.ts
@@ -0,0 +1,26 @@
+export const WP = 'https://lichen.health/wp-content/uploads';
+
+export const logoWhite = `${WP}/2024/10/Lichen_logo_white.svg`;
+export const logoColor = `${WP}/2024/10/Lichen_logo_color.svg`;
+export const galynPhoto = `${WP}/2026/01/Galyn-2.svg`;
+
+export const heroes: Record<string, string> = {
+  home: `${WP}/2024/10/Lichen_Hero.jpg`,
+  about: `${WP}/2024/10/Lichen_About_Hero.png`,
+  'care-model': `${WP}/2024/10/Lichen_Care_hero-1.png`,
+  business: `${WP}/2024/10/Lichen_Business_Hero.png`,
+  platform: `${WP}/2024/10/Lichen_platform_hero.png`,
+  donate: `${WP}/2024/11/Lichen_Hero_Donation.png`,
+  'get-involved': `${WP}/2024/12/Lichen_Hero_Involved.jpg`,
+  check: `${WP}/2024/11/Hero_Check.jpg`,
+  resources: `${WP}/2024/10/Lichen_Hero.jpg`,
+  blog: `${WP}/2024/10/Lichen_Hero.jpg`,
+  events: `${WP}/2024/10/Lichen_Hero.jpg`,
+};
+
+export const socials = {
+  instagram: 'https://www.instagram.com/lichen.health/',
+  facebook: 'https://www.facebook.com/lichen.health',
+  youtube: 'https://www.youtube.com/@LichenHealth',
+  linkedin: 'https://www.linkedin.com/company/lichen-community',
+};
